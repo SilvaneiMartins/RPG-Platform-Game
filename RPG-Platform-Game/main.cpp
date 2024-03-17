7b98786d8af15d073217d6be3e6ffcced2fb6928
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-03-17 10:04:56 -0300

    create variables the game

--- RPG-Platform-Game/main.cpp
+++ RPG-Platform-Game/main.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
+	std::string name = "Silvanei Martins";
+	int health = 100;
+	int damage = 100;
+	float speed = 10.15f;
+	float attackSpeed = 2.5f;
+
+	std::cout << "RPG Platform Game!" << std::endl;
+
+	std::cout << "Nome! " << name << std::endl;
+	std::cout << "Voda! " << health << std::endl;
+	std::cout << "Dano! " << damage << std::endl;
+	std::cout << "Velocidade! " << speed << std::endl;
+	std::cout << "Velocidade do Ataque! " << attackSpeed << std::endl;
 
 	return 0;
 }
